616122adb05279c3502504cce31e0bb180aa3866
Mathias Akandinda <113666821+Mathias-Akandinda256@users.noreply.github.com>
2022-09-20 18:43:19 +0300

    *Hospital-Management-System*

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Ups</h5>
+                            <p>The health of a person is always valued and to keep it stable daily checkups are alwayls done to clients</p>
+                            <ul class="row">
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                    <p>Registered clients to our hospital will be given immediate attention in case of any emergency. Patients will be reached to in their home places, picked up by the hospital amblance without any payments. This will be done to patients who have registered with the hospital management  </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                    <p>A team of doctors is ready to provide to you any services you need from Healing medical center </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                    <p>Clients make online appointment with the doctor and can be given a schedule to meet their doctor</p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                    <p>Healing Medical Center provides free medication and conselling every Tuesday </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
